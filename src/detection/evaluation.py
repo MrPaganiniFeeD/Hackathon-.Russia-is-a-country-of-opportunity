45f687ed5181ae94cdc122939d73b92d20e995a1
mrfrosk <heroker44@gmail.com>
2024-11-08 23:47:07 +0300

    idk

--- src/detection/evaluation.py
+++ src/detection/evaluation.py
@@ -33,7 +33,62 @@ metric_path = "videos/markup_df_train.xlsx"
 gt = pd.read_excel(metric_path)
 sub = simul_sub(gt.copy())
 
+def pre_calc_score(gt, sub):
+  #sub - submission файл
+  #gt - grounded true файл
+  pred_seconds = []
+  correct_predictions = []
+  AE_count_rules_FP = []
+  AE_count_rules_FN = []
 
+  #Проходимся по всем нарушениям из gt
+  for i, r_gt in gt.iterrows():
+    #Берем строки из sub, которые относятся к одному видео и имеют нарушение того же правила, что и анализируемое нарушение в строке gt
+    video_sub = sub[(sub['номер видео'] == r_gt['номер видео']) & (sub['наименование нарушения'] == r_gt['наименование нарушения'])]
+    #Берем строки из gt, которые относятся к одному видео и имеют нарушение того же правила, что и анализируемое нарушение в строке gt
+    video_gt = gt[(gt['номер видео'] == r_gt['номер видео']) & (gt['наименование нарушения'] == r_gt['наименование нарушения'])]
+
+    #Обработка случая, когда есть нарушения правила в grounded true, а в submission его нарушений нет
+    if len(video_sub)==0:
+      #Если в предсказании не было найдено нарушеного правила, хотя на самом деле оно было, то correct_prediction = False
+      pred_seconds.append(np.NaN)
+      correct_predictions.append(False)
+
+      #В предсказании было больше нарушений, чем есть на самом деле
+      FP = max(0 - len(video_gt), 0) # всегда будет 0
+      #В предсказании было меньше нарушений, чем есть на самом деле
+      FN = abs(min(0 - len(video_gt), 0))
+
+      AE_count_rules_FP.append(FP)
+      AE_count_rules_FN.append(FN)
+      continue
+
+    true_sec = r_gt['время нарушения (в секундах)']
+
+    #Берем ближайшую pred сенунду к true секунде для взятого правила
+    pred_sec = min(video_sub['время нарушения (в секундах)'].values, key=lambda x: abs(x - true_sec))
+    pred_seconds.append(pred_sec)
+
+    #При попадание в интервал 5 секунд от фактического нарушения зачисляется предсказание
+    correct_prediction = np.abs(pred_sec-true_sec)<5
+    correct_predictions.append(correct_prediction)
+
+
+    #В предсказании было больше нарушений, чем есть на самом деле
+    FP = max(len(video_sub) - len(video_gt), 0)
+    #В предсказании было меньше нарушений, чем есть на самом деле
+    FN = abs(min(len(video_sub) - len(video_gt), 0))
+
+
+    AE_count_rules_FP.append(FP)
+    AE_count_rules_FN.append(FN)
+
+
+  gt['pred_seconds'] = pred_seconds
+  gt['Корректность прдсказания'] = correct_predictions
+  gt['В предсказании было больше нарушений на кол-во'] = AE_count_rules_FP
+  gt['В предсказании было меньше нарушений на кол-во'] = AE_count_rules_FN
+  return gt
 
 
 
